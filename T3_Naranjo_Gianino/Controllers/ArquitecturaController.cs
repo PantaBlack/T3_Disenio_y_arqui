73fe32826a0bc40cf1e5de79aca65a79b84b32b6
PantaBlack <gianino122@gmail.com>
2026-06-18 04:19:53 +0200

    Tercer commit

---
+++ T3_Naranjo_Gianino/Controllers/ArquitecturaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace T3_Naranjo_Gianino.Controllers
+{
+    public class ArquitecturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ T3_Naranjo_Gianino/Controllers/DisenoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace T3_Naranjo_Gianino.Controllers
+{
+    public class DisenoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
